475252f0ae08d586ed3c6e22ec0f32d31f6d7cef
IvanFranCOCK <49644012+Vitya77@users.noreply.github.com>
2024-08-04 09:52:31 +0300

    Add ability to login with google

--- src/components/Auth/SocialLogin.tsx
+++ src/components/Auth/SocialLogin.tsx
@@ -2,18 +2,69 @@ import { FaFacebookF } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 import { ButtonBase } from "common/ui";
+import { GoogleOAuthProvider, GoogleLogin, useGoogleLogin } from '@react-oauth/google';
+import { useAuth } from "contexts/AuthContext";
+import { useAuthNotificationContext } from "contexts/AuthNotificationContext";
+import { useRouter } from "next/navigation";
 
 export type SocialLoginProps = {
   onGoogleClick?: () => void;
   onFBClick?: () => void;
 };
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
 export function SocialLogin({ onFBClick, onGoogleClick }: SocialLoginProps) {
+  const { setIsOpen, setTitle } = useAuthNotificationContext();
+  const { login } = useAuth();
+  
+  const router = useRouter();
+
+  const handleLoginSuccess = async (response: any) => {
+    try {
+      const { credential } = response;
+      fetch(`${apiUrl}/api/account/auth/google/`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          token: credential,
+        })
+      }).then(response => {
+        if (response.status === 200) {
+            return response.json()
+        }
+        else {
+            return;
+        }
+      })
+      .then(data => {
+        login(data.access, data.refresh);
+        setTitle(data.email);
+        setIsOpen(true);
+        router.push('/account');
+      });
+    } catch (error) {
+      console.error('Google login error', error);
+    }
+  };
+
+  const handleLoginFailure = () => {
+    console.error('Google login failed');
+  };
+
+  const loginGoogle = useGoogleLogin({
+    onSuccess: handleLoginSuccess,
+    onError: handleLoginFailure,
+    flow: 'auth-code',
+  });
+
   return (
     <>
       {onGoogleClick && (
-        <ButtonBase
-          onClick={() => onGoogleClick()}
+          <ButtonBase
+          onClick={loginGoogle}
           className={{
             button:
               "flex items-center justify-center gap-x-2.5 rounded-full border border-tep_gray-700/20 px-8 py-4 text-sm font-bold transition-colors hover:border-black",
